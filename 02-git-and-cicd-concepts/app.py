d4bac0cb30afbc253141871f7c735576caff8681
Ruben Htun <rubenhtun2003@gmail.com>
2026-01-07 12:42:37 +0700

    feat: add dynamic background color based on environment variable

--- 02-git-and-cicd-concepts/app.py
+++ 02-git-and-cicd-concepts/app.py
@@ -6,16 +6,21 @@ app = Flask(__name__)
 ENV_TYPE = os.getenv('ENV_TYPE', 'Development')
 APP_VERSION = "v1.0"
 DB_HOST = os.getenv('DB_HOST', 'Not Connected')
+APP_COLOR = os.getenv('APP_COLOR', 'white')
 
 @app.route('/')
 def home():
     return f"""
-    <h1>DevOps Odyssey - {ENV_TYPE} Environment</h1>
-    <p><b>Status:</b> Deployment Successful!</p>
-    <p><b>Version:</b> {APP_VERSION}</p>
-    <p><b>Connected Host:</b> {DB_HOST}</p>
-    <hr>
-    <p>This application demonstrates the <b>'Build Once, Run Anywhere'</b> principle of CI/CD.</p>
+    <html>
+        <body style="background-color:{APP_COLOR}; font-family: sans-serif; padding: 50px;">
+            <h1>DevOps Odyssey - {ENV_TYPE} Environment</h1>
+            <p><b>Status:</b> Deployment Successful!</p>
+            <p><b>Version:</b> {APP_VERSION}</p>
+            <p><b>Connected Host:</b> {DB_HOST}</p>
+            <hr>
+            <p>This application demonstrates the <b>'Build Once, Run Anywhere'</b> principle of CI/CD.</p>
+        </body>
+    </html>
     """
 
 if __name__ == '__main__':
