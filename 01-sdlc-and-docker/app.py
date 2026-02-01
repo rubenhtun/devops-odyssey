b78265d92eeca4e6ecdb7df888ed734f1a38ea75
Ruben Htun <rubenhtun2003@gmail.com>
2026-02-01 09:14:23 +0700

    modified some html codes and added some extra clear points

--- 01-sdlc-and-docker/app.py
+++ 01-sdlc-and-docker/app.py
@@ -9,7 +9,15 @@ def hello():
             <h1>Welcome to the DevOps Odyssey!</h1>
             <p>This application is running inside a Docker container.</p>
             <hr>
-            <p>Explore the journey of DevOps and containerization.</p>
+            <p>This lab primarily focuses on software development lifecycle (SDLC) and containerization:</p>
+            <ul>
+                <li>Understanding the SDLC process</li>
+                <li>Introduction to Docker and containerization</li>
+                <li>Building and running Docker containers</li>
+                <li>Best practices for containerized applications</li>
+            </ul>
+            <hr>
+            <p><strong>Source Code:</strong> <a href="https://github.com/rubenhtun/devops-odyssey/tree/main/01-sdlc-and-docker" target="_blank">View on GitHub</a></p>
         </body>
     </html>
     """
