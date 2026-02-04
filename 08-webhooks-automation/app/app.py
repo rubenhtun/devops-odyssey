a16534cabad759c0cbf31b58639a15674272a4e3
Ruben Htun <rubenhtun2003@gmail.com>
2026-02-04 15:15:13 +0700

    Update Flask app UI and fix variable naming

--- 08-webhooks-automation/app/app.py
+++ 08-webhooks-automation/app/app.py
@@ -1,1 +1,17 @@
-from Flask
+import os
+import socket
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+@app.route('/')
+def index():
+    context = {
+      'branch_name': os.getenv('BRANCH_NAME', 'N/A'),
+      'build_num': os.getenv('BUILD_NUMBER', 'N/A'),
+      'hostname': socket.gethostname(),
+      'status': 'Healthy'
+    }
+    return render_template('index.html', **context)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
